0642f0534234fd0ac2af914da747b74139bf974e
Andriy Zarevych <39939977+zarevych@users.noreply.github.com>
2020-04-22 00:38:31 +0300

    Create Check-PSAdminMode.ps1

---
+++ Check-PSAdminMode.ps1
@@ -0,0 +1,28 @@
+﻿<#
+
+In Powershell 4.0 you can use requires at the top of your script:
+#Requires -RunAsAdministrator
+
+#>
+
+function Check-PSAdminPermission (){
+   if (-NOT ([Security.Principal.WindowsPrincipal]`
+      [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+      [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+         Return $false
+   }
+   else {
+         Return $true
+   }
+}
+ 
+Write-Host "Checking for administrative privileges …"
+if (Check-PSAdminPermission) {
+    Write-Host "Administrator permission detected" -ForegroundColor Green
+}
+else {
+   Write-Host "Not enough rights to run this script `nOpen the PowerShell console with administrator privileges and run the script again" -ForegroundColor Yellow
+    Break
+}
+ 
+Write-Host "Continue the script..."
